1b3ad8a052bc7d4144bd423912f35a626db29fad
Zwavhudi <zwavhudimulelu@gmail.com>
2020-02-11 11:14:57 +0200

    main route to see if node server was deployed

--- Express/mainRoutes.js
+++ Express/mainRoutes.js
@@ -1,6 +1,11 @@
 const path = require('path')
 let express = require('express')
 let mainRouter = express.Router()
+
+mainRouter.get('/', function (req, res) {
+  res.send("Hello World, I'm Node.js")
+})
+
 mainRouter.get('/', function (req, res) {
   res.send('Hello World')
 })
