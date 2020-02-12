5587563c6dd2daf6d8c7ddfd5985c0bfc17c8261
Zwavhudi <zwavhudimulelu@gmail.com>
2020-02-12 20:56:23 +0200

    changde router

--- mainRoutes.js
+++ mainRoutes.js
@@ -2,9 +2,9 @@ const path = require('path')
 let express = require('express')
 let mainRouter = express.Router()
 
-mainRouter.get('/', function (req, res) {
-  res.send("Hello World, I'm Node.js")
-})
+// mainRouter.get('/', function (req, res) {
+//   res.send("Hello World, I'm Node.js")
+// })
 
 mainRouter.get('/', function (req, res) {
   res.send('Hello World')
