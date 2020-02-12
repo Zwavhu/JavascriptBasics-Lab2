68fd9a25cf5e1b5584d30ad8d4e645e5674cf9ee
Zwavhudi <zwavhudimulelu@gmail.com>
2020-02-12 20:51:33 +0200

    changed mainRouter

--- Express/mainRoutes.js
+++ Express/mainRoutes.js
@@ -1,17 +1,17 @@
-const path = require('path')
-let express = require('express')
-let mainRouter = express.Router()
+const path = require("path");
+let express = require("express");
+let mainRouter = express.Router();
 
-mainRouter.get('/', function (req, res) {
-  res.send("Hello World, I'm Node.js")
-})
+// mainRouter.get('/', function (req, res) {
+//   res.send("Hello World, I'm Node.js")
+// })
 
-mainRouter.get('/', function (req, res) {
-  res.send('Hello World')
-})
+mainRouter.get("/", function(req, res) {
+  res.send("Hello World");
+});
 
-mainRouter.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
+mainRouter.get("/about", function(req, res) {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
 
-module.exports = mainRouter
+module.exports = mainRouter;
